2a6d67f01b6314df1c209581a0ee31de53cf31a3
moonjupark <moonjupark@gmail.com>
2024-02-06 15:22:52 +0900

    Update main.c

--- xv6/main.c
+++ xv6/main.c
@@ -43,7 +43,7 @@ main(void)
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   pci_init();
   arp_scan();
-  i8254_recv();
+  //i8254_recv();
   userinit();      // first user process
 
   mpmain();        // finish this processor's setup
